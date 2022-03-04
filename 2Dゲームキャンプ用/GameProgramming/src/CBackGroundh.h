4e1ca2c180973fcf2803c91945ad001b07e81c56
Keigo-Fukusima-Fukuyama <21010514@anabuki-net.ne.jp>
2022-03-04 12:28:45 +0900

    仮ステージ背景

---
+++ 2Dゲームキャンプ用/GameProgramming/src/CBackGroundh.h
@@ -0,0 +1,18 @@
+#ifndef CBACKGROUND_H
+#define CBACKGROUND_H
+
+#include "CRectangle.h"
+#include "CSceneGame.h"
+
+class CBuckGround : public CRectangle{
+public:
+	void Render();
+	CBuckGround();
+private:
+	CTexture BuckGround;
+};
+
+
+#endif //èIóπ
+
+
