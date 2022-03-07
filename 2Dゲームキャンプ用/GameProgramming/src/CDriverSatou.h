90c97988b915f66d5a1654915c920007bb7ccecc
satou-kousuke <21010504@anabuki-net.ne.jp>
2022-03-07 17:12:02 +0900

    CDriverSatou

--- 2Dゲームキャンプ用/GameProgramming/src/CDriverSatou.h
+++ 2Dゲームキャンプ用/GameProgramming/src/CDriverSatou.h
@@ -1,40 +1,43 @@
 #ifndef CDRIVERSATOU_H
 #define CDRIVERSATOU_H
+#include "CPlayer.h"
+
 
 class CDriverSatou
 {
-pablic :
+public:
+	int mRight;
+
 	void Init()
 	{
+
 		
-		//画面範囲右の設定
-		mRight = mLeft + WTDTH;
-		//画面下の設定
-		//mBottom = CPlayer::spInstance->y - HEIGHT / 2;
-		mBottom = -500;
-		//画面範囲上の設定
-		mTop = mBottom + HEIGHT;
+	//	//画面範囲右の設定
+	//	mRight = mLeft + WTDTH;
+	//	//画面下の設定
+	//	//mBottom = CPlayer::spInstance->y - HEIGHT / 2;
+	//	mBottom = -500;
+	//	//画面範囲上の設定
+	//	mTop = mBottom + HEIGHT;
 
-		/*画面の投影変更開始*/
-		glMatrixMode(GL_PROJECTION);
-		glLoadIdentity();//行列（設定）を初期化
-		//2Dの投影範囲の設定
-		gluOrtho2D(mLeft, mRight, mBottom, mTop);
-		for (int i = 0; i < VectorRect.size(); i++) {
-			//描画処理
-			VectorRect[i]->Render();
-		}
-		glLoadIdentity();//行列（設定）を初期化
-	//2Dの投影範囲を設定
-		gluOrtho2D(-950, 950, -500, 500);
-	}
+	//	/*画面の投影変更開始*/
+	//	glMatrixMode(GL_PROJECTION);
+	//	glLoadIdentity();//行列（設定）を初期化
+	//	//2Dの投影範囲の設定
+	//	gluOrtho2D(mLeft, mRight, mBottom, mTop);
+	//	for (int i = 0; i < VectorRect.size(); i++) {
+	//		//描画処理
+	//		VectorRect[i]->Render();
+	//	}
+	//	glLoadIdentity();//行列（設定）を初期化
+	////2Dの投影範囲を設定
+	//	gluOrtho2D(-950, 950, -500, 500);
+	};
 	void Update() {
-		//画面範囲左の設定
-		if (3300 > mLeft) {
 
-			mLeft++;
-
-		}
+		//if (mLeft + 30 > CPlayer::spInstance->x) {
+		//	CPlayer::spInstance->x = mLeft + 30;
+		//};
 	}
 };
 
