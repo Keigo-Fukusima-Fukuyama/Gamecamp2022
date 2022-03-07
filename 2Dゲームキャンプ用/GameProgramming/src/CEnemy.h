0cc0a0b63fc79bb158866fdfd2d4c4566e16e4fd
Keigo-Fukusima-Fukuyama <21010514@anabuki-net.ne.jp>
2022-03-07 12:34:11 +0900

    敵

--- 2Dゲームキャンプ用/GameProgramming/src/CEnemy.h
+++ 2Dゲームキャンプ用/GameProgramming/src/CEnemy.h
@@ -6,13 +6,7 @@
 
 class CEnemy : public CRectangle {
 public:
-	int mFx; //X軸方向の移動 -1:左 0:移動しない 1:右へ移動
-	int mFy; //Y軸方向の移動 -1:下 0:移動しない 1:上へ移動
-//	bool mEnabled;	//有効フラグ
-
-//37	CBullet EBullet[20];
-	int mFireCount;
-
+	
 	CEnemy();
 
 	void Update();
@@ -21,5 +15,11 @@ public:
 
 	//36
 	void Collision(CRectangle *i, CRectangle *y);
+
+private:
+	int mFx; //X軸方向の移動 -1:左 0:移動しない 1:右へ移動
+	int mFy; //Y軸方向の移動 -1:下 0:移動しない 1:上へ移動
+	int mFireCount;
+	int m_Hp; //HP
 };
 #endif
