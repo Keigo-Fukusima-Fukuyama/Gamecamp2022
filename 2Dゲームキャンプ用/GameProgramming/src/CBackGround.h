ca66180184de7f9840132244243bd0c8beba607d
Keigo-Fukusima-Fukuyama <21010514@anabuki-net.ne.jp>
2022-03-09 15:00:52 +0900

    背景画像の追加　描画　敵のアニメーションの完成(スライム)

--- 2Dゲームキャンプ用/GameProgramming/src/CBackGround.h
+++ 2Dゲームキャンプ用/GameProgramming/src/CBackGround.h
@@ -16,6 +16,33 @@ public:
 private:
 	CTexture mpBackGround;
 };
+
+class CBackGround2 : public CRectangle {
+public:
+
+	void Render();
+
+	CBackGround2();
+
+	static CBackGround2* spInstance;
+
+private:
+	CTexture mBackGround2;
+};
+
+class CBackGround3 : public CRectangle {
+public:
+
+	void Render();
+
+	CBackGround3();
+
+	static CBackGround3* spInstance;
+
+private:
+	CTexture mBackGround3;
+};
+
 #endif //èIóπ
 
 
